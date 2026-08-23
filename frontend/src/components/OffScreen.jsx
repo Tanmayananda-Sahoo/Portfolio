13b0f8593f6a040ca05b55bde2353e75ac78b207
Tanmayananda-Sahoo <boyvector06@gmail.com>
2026-08-23 21:23:08 +0530

    small changes

--- frontend/src/components/OffScreen.jsx
+++ frontend/src/components/OffScreen.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import CardScatter from "./CardScatter.jsx";
 
 const dates = [
-  { id: 1, src:"https://images.unsplash.com/photo-1616002411355-49593fd89721?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-  { id: 2, src:"https://images.unsplash.com/photo-1616002411355-49593fd89721?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-  { id: 3, src:"https://images.unsplash.com/photo-1616002411355-49593fd89721?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-  { id: 4, src:"https://images.unsplash.com/photo-1616002411355-49593fd89721?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-  { id: 5, src:"https://images.unsplash.com/photo-1616002411355-49593fd89721?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+  { id: 1, src:"../src/assets/me.jpg"},
+  { id: 2, src:"../src/assets/aesthetic.jpeg"},
+  { id: 3, src:"../src/assets/me.jpg"},
+  { id: 4, src:"../src/assets/me.jpg"},
+  { id: 5, src:"../src/assets/me.jpg"},
 ];
 
 const OffScreen = () => {
